6369d1a243ffb7edf9c426d8d471c4dc01a3f22d
Boris Krasnov <crasnov.boris@yandex.ru>
2023-07-26 13:31:30 +0300

    Сделали рефакторинг кода

--- src/Controller/PersonalAccountController.php
+++ src/Controller/PersonalAccountController.php
@@ -2,12 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\ServiceInfo;
-use App\Entity\Transaction;
-use App\Repository\ServiceInfoRepository;
 use App\Repository\UserAccountRepository;
-use DateTime;
-use DateTimeImmutable;
+use App\Service\UserAccountService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,123 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class PersonalAccountController extends AbstractController
 {
 
+
     #[Route('/')]
     public function myServices(UserAccountRepository $userAccountRepository,
-                               ServiceInfoRepository $serviceInfoRepository,
-                               Request $request ): Response
+                               Request $request,
+                               UserAccountService $userAccountService ): Response
     {
         $currentUser = $userAccountRepository->find(1);
 
         if ($request->getMethod() === 'POST')
         {
-
-            // Получили имя сервиса
-            $serviceName = $request->request->all()['service-name'];
-
-            // Количество указанного в сервисе
-            $amountOfService = $request->request->all()['number-of-service'];
-
-            // Получили цену данного наименования
-            $priceOfSelectedService = $serviceInfoRepository->findServiceByNameAndPriceNotNull($serviceName)->getPrice();
-
-            $todayDate = new DateTimeImmutable();
-            $firstNumberNextMonth = new DateTime();
-            $firstNumberNextMonth->setTimestamp(strtotime("first day of next month"));
-            $diff = $firstNumberNextMonth->diff($todayDate)->format("%a");
-            $daysInMonth = date('t');
-
-            // умножили количество сервисов на цену выбранного сервиса и разделили на дни до первого числа
-            $totalCoast = ceil($amountOfService * $priceOfSelectedService /  $daysInMonth * $diff );
-
-            // Проверка что больше итоговая сумма или баланс пользователя
-            if ($currentUser->getBalance() < $totalCoast)
-            {
-                $this->addFlash('warning','Ваш баланс меньше чем нужная сумма');
-                $this->redirectToRoute('services');
-            }else{
-
-                // Ищем все сервисы которые есть у нашего пользователя
-                $servicesOfUser = $serviceInfoRepository->findServicesNamesByUser($currentUser->getId());
-
-                // Переменная в которую попадает значение в случае если имя сервиса уже есть в пользователе
-                $checkSameNames = null;
-
-
-                // Проверяем если ли у нашего пользователя сервисы вообще
-                if (!empty($servicesOfUser))
-                {
-
-                    // Перебираем все сервисы пользователя
-                    foreach ($servicesOfUser as $serviceOfUser)
-                    {
-                        // Если находим уже имеющийся сервис, тогда изменяем его параметры
-                        $serviceOfUser->getName() === $serviceName ? $checkSameNames = $serviceOfUser->getId() : $checkSameNames = null;
-                        if ($checkSameNames)
-                        {
-                            break;
-                        }
-                    }
-
-                    if ($checkSameNames)
-                    {
-                        $serviceOfUserById = $serviceInfoRepository->find($checkSameNames);
-
-                        // Обновляем количество сервисов
-                        $newAmountOfService = $serviceOfUserById->getAmount() + $amountOfService;
-
-                        // Задаем новое значение в колонку количество
-                        $serviceOfUserById->setAmount($newAmountOfService);
-
-                        // Задаем новый баланс с вычетом
-                        $currentUser->setBalance($currentUser->getBalance() - $totalCoast);
-
-                        // Задаем нашему пользователю обновленный сервис
-                        $currentUser->addUserService($serviceOfUserById);
-
-                        // Обновляем пользователя
-                        $userAccountRepository->save($currentUser,true);
-
-                    // Если данного сервиса еще не было в сущности User, то создаем
-                    }else{
-                    $newServiceInfo = new ServiceInfo();
-
-                    $newServiceInfo->setPrice($priceOfSelectedService);
-                    $newServiceInfo->setAmount($amountOfService);
-                    $newServiceInfo->setName($serviceName);
-                    $currentUser->setBalance($currentUser->getBalance() - $totalCoast);
-
-                    $currentUser->addUserService($newServiceInfo);
-
-                    $userAccountRepository->save($currentUser,true);
-                }
-                // Если список пуст, то добавляем новый сервис
-                }else{
-                    $newServiceInfo = new ServiceInfo();
-                    $newServiceInfo->setPrice($priceOfSelectedService);
-                    $newServiceInfo->setAmount($amountOfService);
-                    $newServiceInfo->setName($serviceName);
-
-                    $currentUser->setBalance($currentUser->getBalance() - $totalCoast);
-
-                    $currentUser->addUserService($newServiceInfo);
-
-                    $userAccountRepository->save($currentUser,true);
-                }
-
-                $this->addFlash('success','Cервис успешно добавлен');
-                $this->redirectToRoute('services');
-            }
-
-            $transaction = new Transaction();
-            $transaction->setServiceName($serviceName);
-            $transaction->setDate(new \DateTimeImmutable());
-            $transaction->setTotalPrice($totalCoast);
-            $transaction->setAccountBalance($currentUser->getBalance());
-            $currentUser->addUserTransaction($transaction);
-            $userAccountRepository->save($currentUser,true);
+            $userAccountService->addAndShowUserServices($request,$currentUser);
         }
-
-
 
         return $this->render('personal-account/my-services.html.twig',[
             'user' => $currentUser,
@@ -142,17 +33,13 @@ class PersonalAccountController extends AbstractController
 
     #[Route('/transactions')]
     public function myTransactions(UserAccountRepository $userAccountRepository,
-                                  Request $request): Response
+                                  Request $request, UserAccountService $userAccountService): Response
     {
         $currentUser = $userAccountRepository->find(1);
 
         if ($request->getMethod() === 'POST')
         {
-            $newBalance = $currentUser->getBalance() + $request->request->all()['amount-to-add'];
-            $currentUser->setBalance($newBalance);
-            $userAccountRepository->save($currentUser,true);
-            $this->addFlash('success','Баланс пополнен успешно');
-            $this->redirectToRoute('transactions');
+            $userAccountService->addMoneyToUserBalance($request,$currentUser);
         }
 
         return $this->render('personal-account/my-transactions.html.twig',[
@@ -161,62 +48,15 @@ class PersonalAccountController extends AbstractController
     }
 
     #[Route('/settlement-day')]
-    public function settlementDay(UserAccountRepository $userAccountRepository): Response
+    public function settlementDay(UserAccountService $userAccountService): Response
     {
-        $currentUser = $userAccountRepository->find(1);
-
-        $userServices = $currentUser->getUserServices();
-        $totalPayment = 0;
-
-        $todayDate = new DateTimeImmutable();
-        $firstNumberNextMonth = new DateTime();
-        $firstNumberNextMonth->setTimestamp(strtotime("first day of next month"));
-        $diff = $firstNumberNextMonth->diff($todayDate)->format("%a");
-        $dayInMonth = date('t');
-
-
-        foreach ($userServices as $service)
-        {
-            $totalPayment += ceil($service->getPrice() * $service->getAmount() / $dayInMonth * $diff);
-        }
-
-        if ($totalPayment > $currentUser->getBalance())
-        {
-            $this->addFlash('warning','Недостаточно средства для оплаты всех услуг');
-            return $this->redirectToRoute('transactions');
-        }
-
-
-        $currentUser->setBalance($currentUser->getBalance() - $totalPayment);
-        $userAccountRepository->save($currentUser,true);
-        $this->addFlash('success','деньги за все услуги успешно списаны');
-        return $this->redirectToRoute('transactions');
-
+        return $userAccountService->immitateSettlementDay();
     }
 
     #[Route('/deleteservice/{id<\d+>}')]
-    public function deleteService(int $id,
-                             ServiceInfoRepository $serviceInfoRepository,
-                             UserAccountRepository $userAccountRepository): Response
+    public function deleteService(int $id,UserAccountService $userAccountService): Response
     {
-        $currentUser = $userAccountRepository->find(1);
-
-        $todayDate = new DateTimeImmutable();
-        $firstNumberNextMonth = new DateTime();
-        $firstNumberNextMonth->setTimestamp(strtotime("first day of next month"));
-        $diff = $firstNumberNextMonth->diff($todayDate)->format("%a");
-        $daysInMonth = date('t');
-
-        $chooseService = $serviceInfoRepository->find($id);
-        $servicePrice = $chooseService->getPrice();
-        $serviceAmount = $chooseService->getAmount();
-        $totalCoast = round($serviceAmount * $servicePrice / $daysInMonth * $diff );
-        $currentUser->setBalance($currentUser->getBalance() + $totalCoast);
-        $serviceInfoRepository->remove($chooseService,true);
-
-        $this->addFlash('success','Cервис успешно удален');
-        return $this->redirectToRoute('services');
-
+        return $userAccountService->deleteServiceById($id);
     }
 
 
---
+++ src/Service/UserAccountService.php
@@ -0,0 +1,213 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\ServiceInfo;
+use App\Entity\Transaction;
+use App\Entity\UserAccount;
+use App\Repository\ServiceInfoRepository;
+use App\Repository\UserAccountRepository;
+use DateTime;
+use DateTimeImmutable;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+
+class UserAccountService
+{
+    public function __construct(private UserAccountRepository $userAccountRepository,
+                                private ServiceInfoRepository $serviceInfoRepository,
+                                private RequestStack $requestStack,
+                                private RouterInterface $router,
+                                )
+    {
+    }
+
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function addAndShowUserServices(Request $request,UserAccount $userAccount): void
+    {
+
+        // Получили имя сервиса
+        $serviceName = $request->request->all()['service-name'];
+
+        // Количество указанного в сервисе
+        $amountOfService = $request->request->all()['number-of-service'];
+
+        // Получили цену данного наименования
+        $priceOfSelectedService = $this->serviceInfoRepository->findServiceByNameAndPriceNotNull($serviceName)->getPrice();
+
+        $compareTime = $this->compareTime();
+
+        // умножили количество сервисов на цену выбранного сервиса и разделили на дни до первого числа
+        $totalCoast = ceil($amountOfService * $priceOfSelectedService /  $compareTime[0] * $compareTime[1] );
+
+        // Проверка что больше итоговая сумма или баланс пользователя
+        if ($userAccount->getBalance() < $totalCoast)
+        {
+            //$this->addFlash('warning','Ваш баланс меньше чем нужная сумма');
+            $this->requestStack->getSession()->getFlashBag()->add('warning','Ваш баланс меньше чем нужная сумма');
+            //$this->redirectToRoute('services');
+            new RedirectResponse($this->router->generate('services'),302);
+        }else{
+
+            // Ищем все сервисы которые есть у нашего пользователя
+            $servicesOfUser = $this->serviceInfoRepository->findServicesNamesByUser($userAccount->getId());
+
+            // Переменная в которую попадает значение в случае если имя сервиса уже есть у пользователя
+            $checkSameNames = null;
+
+            // Проверяем если ли у нашего пользователя сервисы вообще
+            if (!empty($servicesOfUser))
+            {
+
+                // Перебираем все сервисы пользователя
+                foreach ($servicesOfUser as $serviceOfUser)
+                {
+                    // Если находим уже имеющийся сервис, тогда изменяем его параметры
+                    $serviceOfUser->getName() === $serviceName ? $checkSameNames = $serviceOfUser->getId() : $checkSameNames = null;
+                    if ($checkSameNames)
+                    {
+                        break;
+                    }
+                    }
+
+                    if ($checkSameNames)
+                    {
+                        $serviceOfUserById = $this->serviceInfoRepository->find($checkSameNames);
+
+                        // Обновляем количество сервисов
+                        $newAmountOfService = $serviceOfUserById->getAmount() + $amountOfService;
+
+                        // Задаем новое значение в колонку количество
+                        $serviceOfUserById->setAmount($newAmountOfService);
+
+                        // Задаем новый баланс с вычетом
+                        $userAccount->setBalance($userAccount->getBalance() - $totalCoast);
+
+                        // Задаем нашему пользователю обновленный сервис
+                        $userAccount->addUserService($serviceOfUserById);
+
+                        // Обновляем пользователя
+                        $this->userAccountRepository->save($userAccount,true);
+
+                        // Если данного сервиса еще не было в сущности User, то создаем
+                    }else{
+                        $this->makeNewUserService($priceOfSelectedService,$amountOfService,$serviceName,
+                                                    $totalCoast,$userAccount);
+                    }
+                    // Если список пуст, то добавляем новый сервис
+                }else{
+                    $this->makeNewUserService($priceOfSelectedService,$amountOfService,$serviceName,
+                                        $totalCoast,$userAccount);
+            }
+
+                //$this->addFlash('success','Cервис успешно добавлен');
+                $this->requestStack->getSession()->getFlashBag()->add('success','Сервис успешно добавлен');
+                //$this->redirectToRoute('services');
+                new RedirectResponse($this->router->generate('services'),302);
+            }
+
+            $transaction = new Transaction();
+            $transaction->setServiceName($serviceName);
+            $transaction->setDate(new DateTimeImmutable());
+            $transaction->setTotalPrice($totalCoast);
+            $transaction->setAccountBalance($userAccount->getBalance());
+            $userAccount->addUserTransaction($transaction);
+            $this->userAccountRepository->save($userAccount,true);
+        }
+
+    public function addMoneyToUserBalance(Request $request,UserAccount $currentUser): void
+    {
+        $newBalance = $currentUser->getBalance() + $request->request->all()['amount-to-add'];
+        $currentUser->setBalance($newBalance);
+        $this->userAccountRepository->save($currentUser,true);
+        $this->requestStack->getSession()->getFlashBag()->add('success','Баланс успешно пополнен');
+        new RedirectResponse($this->router->generate('transactions'),302);
+    }
+
+    public function immitateSettlementDay(): RedirectResponse
+    {
+        $currentUser = $this->userAccountRepository->find(1);
+
+        $userServices = $currentUser->getUserServices();
+        $totalPayment = 0;
+
+        $todayDate = new DateTimeImmutable();
+        $firstNumberNextMonth = new DateTime();
+        $firstNumberNextMonth->setTimestamp(strtotime("first day of next month"));
+        $diff = $firstNumberNextMonth->diff($todayDate)->format("%a");
+        $dayInMonth = date('t');
+
+
+        foreach ($userServices as $service)
+        {
+            $totalPayment += ceil($service->getPrice() * $service->getAmount() / $dayInMonth * $diff);
+        }
+
+        if ($totalPayment > $currentUser->getBalance())
+        {
+            $this->requestStack->getSession()->getFlashBag()->add('warning','Недостаточно средств для оплаты всех услуг');
+            return new RedirectResponse($this->router->generate('transactions'),302);
+        }
+
+
+        $currentUser->setBalance($currentUser->getBalance() - $totalPayment);
+        $this->userAccountRepository->save($currentUser,true);
+        $this->requestStack->getSession()->getFlashBag()->add('success','деньги за все услуги успешно списаны');
+        return new RedirectResponse($this->router->generate('transactions'),302);
+    }
+
+    public function deleteServiceById($id): RedirectResponse
+    {
+        $currentUser = $this->userAccountRepository->find(1);
+
+        $todayDate = new DateTimeImmutable();
+        $firstNumberNextMonth = new DateTime();
+        $firstNumberNextMonth->setTimestamp(strtotime("first day of next month"));
+        $diff = $firstNumberNextMonth->diff($todayDate)->format("%a");
+        $daysInMonth = date('t');
+
+        $chooseService = $this->serviceInfoRepository->find($id);
+        $servicePrice = $chooseService->getPrice();
+        $serviceAmount = $chooseService->getAmount();
+        $totalCoast = round($serviceAmount * $servicePrice / $daysInMonth * $diff );
+        $currentUser->setBalance($currentUser->getBalance() + $totalCoast);
+        $this->serviceInfoRepository->remove($chooseService,true);
+
+        $this->requestStack->getSession()->getFlashBag()->add('success','Cервис успешно удален');
+        return new RedirectResponse($this->router->generate('services'),302);
+    }
+
+    private function compareTime():array
+    {
+        $todayDate = new DateTimeImmutable();
+        $firstNumberNextMonth = new DateTime();
+        $firstNumberNextMonth->setTimestamp(strtotime("first day of next month"));
+        $diff = $firstNumberNextMonth->diff($todayDate)->format("%a");
+        $daysInMonth = date('t');
+
+        return [$daysInMonth,$diff];
+    }
+
+    private function makeNewUserService(int $priceOfSelectedService,int $amountOfService,string $serviceName,
+                                        int $totalCoast,UserAccount $userAccount): void
+    {
+        $newServiceInfo = new ServiceInfo();
+
+        $newServiceInfo->setPrice($priceOfSelectedService);
+        $newServiceInfo->setAmount($amountOfService);
+        $newServiceInfo->setName($serviceName);
+        $userAccount->setBalance($userAccount->getBalance() - $totalCoast);
+
+        $userAccount->addUserService($newServiceInfo);
+
+        $this->userAccountRepository->save($userAccount,true);
+    }
+}
